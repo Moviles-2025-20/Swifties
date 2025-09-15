edfdb12b08c808e0a94d63f796ebfdde0fe99c5f
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-09-14 20:01:20 -0500

    Project created from XCode

---
+++ SwiftiesApp/SwiftiesApp/SwiftiesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftiesAppApp.swift
+//  SwiftiesApp
+//
+//  Created by NATALIA VILLEGAS CALDERON on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftiesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

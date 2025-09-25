ee9d69f67fc0bffe68e1bd9de6c0b48555a0c774
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-09-24 19:00:27 -0500

    Initial Commit

---
+++ Swifties/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swifties
+//
+//  Created by Natalia Villegas Calderón on 24/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swifties/SwiftiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftiesApp.swift
+//  Swifties
+//
+//  Created by Natalia Villegas Calderón on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

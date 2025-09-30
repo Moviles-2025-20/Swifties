e7b94cef8e1dec38ab98d77486774508bff723e2
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-09-29 19:39:04 -0500

    List view done

---
+++ Swifties/Components/Events/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Swifties
+//
+//  Created by Natalia Villegas Calderón on 28/09/25.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
---
+++ Swifties/Screens/EventsListView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsListView.swift
+//  Swifties
+//
+//  Created by Natalia Villegas Calderón on 28/09/25.
+//
+
+import SwiftUI
+
+struct EventsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsListView()
+}

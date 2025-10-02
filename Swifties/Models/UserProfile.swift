1b510c3d756f28e57b295dab5f00998c5741c5fa
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-10-02 13:15:58 -0500

    Putting Profile on this branch to solve possible conflicts

---
+++ Swifties/Models/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  Swifties
+//
+//  Created by Juan Esteban Vásquez on 01/10/25.
+//
+
+import Foundation
+
+/// Domain model representing a user's profile in the app.
+/// This is the Model in MVVM and should remain a simple data container.
+struct UserProfile: Identifiable, Equatable {
+    let id: String
+    let name: String
+    let avatar_url: String?
+    let major: String
+    let age: Int
+    let indoor_outdoor_score: String
+    let preferences: [String]
+}
---
+++ Swifties/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewModel.swift
+//  Swifties
+//
+//  Created by Natalia Villegas Calderón on 2/10/25.
+//
+
+import SwiftUI
+
+struct ProfileViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileViewModel()
+}

cbe858c08ed9f36e156dc3094e80b803ac8290f2
cflorido <cflorido@uniandes.edu.co>
2025-10-02 19:21:40 -0500

    fix auth

--- Swifties/SwiftiesApp.swift
+++ Swifties/SwiftiesApp.swift
@@ -7,49 +7,50 @@
 
 import SwiftUI
 import FirebaseCore
-import UserNotifications
 
-// MARK: - AppDelegate
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-    
+class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        // Inicializar Firebase
         FirebaseApp.configure()
-        
-        // Configurar notificaciones
-        UNUserNotificationCenter.current().delegate = self
-        NotificationManager.shared.requestAuthorization()
-        
         return true
     }
-    
-    // Restringir la app a modo retrato
-    func application(_ application: UIApplication,
-                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    // Restrict the app to portrait mode only
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return .portrait
     }
-    
-    // Mostrar notificaciones aunque la app esté en primer plano
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .sound, .badge])
-    }
-}
 
-// MARK: - Main App
+
 @main
 struct SwiftiesApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var authViewModel = AuthViewModel()
-    
+
     var body: some Scene {
         WindowGroup {
-            MainView()
-                .environmentObject(authViewModel)
-        }
+            NavigationStack {
+                StartView()
+            }
+            .environmentObject(authViewModel)
+            // BEFORE
+            //@State private var isLoading = true
+
+            //var body: some Scene {
+            //WindowGroup {
+            //if isLoading {
+            //LoadingView()
+            //.onAppear {
+            // Cambiar a EventListView después de 2 segundos
+            //DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            //withAnimation {
+            //    isLoading = false
+            //}
+            //}
+            //}
+            //} else {
+            //HomeView()
+            //EventListView(viewModel: EventListViewModel())
+            //}
+         }
+       }
     }
 }
-

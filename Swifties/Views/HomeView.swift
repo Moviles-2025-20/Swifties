6f7b8e557dba1089f1ff3b0913af52363a04e633
cflorido <cflorido@uniandes.edu.co>
2025-10-04 12:56:21 -0500

    type two question

--- Swifties/Views/HomeView.swift
+++ Swifties/Views/HomeView.swift
@@ -41,9 +41,7 @@ struct HomeView: View {
                             .padding(.bottom, 10)
                             
                             HStack (spacing: 15) {
-                                Button {
-                                    print("Weekly Challenge")
-                                } label: {
+                                NavigationLink(destination: WeeklyChallengeView()) {
                                     Text("Weekly Challenge")
                                         .frame(width: 120, height: 80)
                                         .font(.body.weight(.semibold))

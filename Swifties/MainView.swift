4e68207c527e7b35490e9c50c5b099675e198add
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-10-04 12:41:48 -0500

    Plis work

--- Swifties/MainView.swift
+++ Swifties/MainView.swift
@@ -32,7 +32,7 @@ struct MainView: View {
         case 1:
             EventListView(viewModel: EventListViewModel())
         case 2:
-            Magic8BallView()
+            WishMeLuckView()
         case 3:
             ProfileView()
         default:
